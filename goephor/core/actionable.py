26be56a39645087097840c1b22c44aacf266f269
Ian Itow <ian.itow@isilon.com>
2016-04-12 14:48:01 -0700

    Updates to generic flash file

--- goephor/core/actionable.py
+++ goephor/core/actionable.py
@@ -32,6 +32,7 @@ def create_flash(key, mnt, disk_size, disk_file='/tmp', strict=True, noop=False)
     disk_file = _sanitize(disk_file)
     if os.path.exists(disk_file):
         shell("rm -rf %s" % (disk_file))
+    shell("touch %s" % (disk_file))
     print "mnt:%s" % (mnt)
     disk_size_small = int(disk_size)
     disk_size_small = str(disk_size_small)
