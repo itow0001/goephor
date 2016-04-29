66ec03678b3b088ef86cd60a025fa837e23c1672
Ian Itow <ian.itow@isilon.com>
2016-04-29 16:00:06 -0700

    created setup.py to create a pip package

---
+++ setup.py
@@ -0,0 +1,42 @@
+'''
+Created on Apr 29, 2016
+
+@author: iitow
+'''
+from setuptools import setup, find_packages
+
+SRCDIR = 'goephor'
+
+
+def readme():
+    ''' Spits out README.rst for our long_description
+    with open('README.rst', 'r') as fobj:
+        return fobj.read()
+    '''
+
+
+setup(
+    name='goephor',
+    version='1.0.0',
+    description="Build automation tool",
+    long_description=readme(),
+    author='ian.itow',
+    author_email='ian.itow@isilon.com',
+    url='',
+    license='',
+    classifiers=[
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 2.7',
+    ],
+    package_dir={'': SRCDIR},
+    packages=find_packages(SRCDIR, exclude=['examples']),
+    zip_safe=False,
+    install_requires=[
+        'requests==2.7.0',
+        'PyYAML==3.10'
+    ],
+    entry_points={
+        'console_scripts': ['goephor = goephor.goephor:__main__']
+    },
+    include_package_data=True,
+)
