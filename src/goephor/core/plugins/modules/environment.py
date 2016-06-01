949098c129bb7507953f184f043f198622c04242
iitow <iitow@isilon.com>
2016-06-01 16:48:19 -0700

    DEBUG on Environment variables

--- src/goephor/core/plugins/modules/environment.py
+++ src/goephor/core/plugins/modules/environment.py
@@ -15,7 +15,7 @@ class EnvManager(object):
     when the action obj is initialized
     Its contained within the action_manager.
     '''
-    def __init__(self, debug=False):
+    def __init__(self, debug=True):
         '''
         Constructor
 
