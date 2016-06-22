d709cfd110c9ef933d0f6146a43ac4803b4e8995
iitow <iitow@isilon.com>
2016-06-22 14:37:25 -0700

    testing globals

--- src/tests.py
+++ src/tests.py
@@ -180,6 +180,7 @@ def tests():
     calls all the tests here & collects results
     '''
     tests = []
+    '''
     tests.append(test_condition())
     tests.append(test_defaults())
     tests.append(test_environment())
@@ -192,6 +193,7 @@ def tests():
     tests.append(test_release())
     tests.append(test_string())
     tests.append(test_on_exit())
+    '''
     tests.append(test_include())
     print "\n\n[Test Results]:"
     for test in tests:
