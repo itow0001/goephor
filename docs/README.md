d0c4be458a87de57afd661ad70709a5651752fec
iitow <iitow@isilon.com>
2016-07-01 14:03:56 -0700

    Updates to make pathing generic

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,8 @@
-== COMMAND ==
+##User Tutorial##
+
+##Development Tutorial## 
+
+##Generating A Call Graph##
 * pip install pycallgraph
 ```
 pycallgraph -e argparse* -e gettext* -e posixpath* -e importlib* -e os* -e re* -e UserDict* -e yaml* -e pickle* -e subprocess* -e sre_parse* -e encodings* -e sre_compile* -e codecs* -e atexit* -e shlex* -e pty* -e statement* -e genericpath* -e stat* -e tty* -e locale* graphviz --output-file=../docs/goephor_ex_system.png -- goephor.py -f ./examples/ex_system.yaml -e
--- src/tests.py
+++ src/tests.py
@@ -6,16 +6,37 @@ Created on May 4, 2016
 from goephor.core.plugins.modules.terminal import shell
 import inspect
 import sys
+import argparse
+import platform
 
 
-def test_condition():
+def menu():
+    '''
+    argparse menu here
+    '''
+    parser = argparse.ArgumentParser(
+        description='Testing of goephor')
+    parser.add_argument('-E',
+                        action="store",
+                        dest="envs",
+                        default="",
+                        help='Add env vars delimiter:"," '
+                        'example.'
+                        ' "BASE_PATH=/tmp,WORKPATH=/${BASE_PATH}/addon"')
+    parser.add_argument('--version',
+                        action='version',
+                        version='goephor-test')
+    return parser.parse_args()
+
+
+def test_condition(options):
     '''
     test of core/plugins/condition.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
     output = shell("python -u goephor.py -f "
-                   "./examples/ex_condition.yaml -e").get('stdout')
+                   "./examples/ex_condition.yaml -e -E %s" % options.envs).get('stdout')
     if '(THEN 1)' not in output:
         return {funct: False}
     output = shell("python -u goephor.py -f "
@@ -26,14 +47,14 @@ def test_condition():
     return {funct: True}
 
 
-def test_defaults():
+def test_defaults(options):
     '''
     test of core/plugins/pluginable.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
     output = shell("python -u goephor.py -f "
-                   "./examples/ex_defaults.yaml -e").get('stdout')
+                   "./examples/ex_defaults.yaml -e -E %s" % options.envs).get('stdout')
     if '(PASS)' not in output:
         return {funct: False}
     output = shell("python -u goephor.py -f "
@@ -44,167 +65,167 @@ def test_defaults():
     return {funct: True}
 
 
-def test_environment():
+def test_environment(options):
     '''
     test of core/plugins/environment.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
     output = shell("python -u goephor.py -f "
-                   "./examples/ex_environment.yaml -e").get('stdout')
+                   "./examples/ex_environment.yaml -e -E %s" % options.envs).get('stdout')
     if '(FAIL)' in output:
         return {funct: False}
     return {funct: True}
 
 
-def test_freebsd():
+def test_freebsd(options):
     '''
     test of core/plugins/freebsd.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_freebsd.yaml -e")
-    if not session.get('code') == 0:
-        return {funct: False}
+    if platform.system() == 'FreeBSD':
+        session = shell("python -u goephor.py -f ./examples/ex_freebsd.yaml -e -E %s" % options.envs)
+        if not session.get('code') == 0:
+            return {funct: False}
     return {funct: True}
 
 
-def test_http():
+def test_http(options):
     '''
     test of core/plugins/http.py
     '''
     funct = inspect.stack()[0][3]
     print "\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_http.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_http.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
 
-def test_receipt():
+def test_receipt(options):
     '''
     test of core/plugins/receipt.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_receipt.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_receipt.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
 
-def test_remote():
+def test_remote(options):
     '''
     test of core/plugins/remote.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_remote.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_remote.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
 
-def test_scm():
+def test_scm(options):
     '''
     test of core/plugins/scm.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_scm.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_scm.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
 
-def test_system():
+def test_system(options):
     '''
     test of core/plugins/system.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_system.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_system.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
 
-def test_release():
+def test_release(options):
     '''
     test of core/plugins/release.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_release.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_release.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
-def test_string():
+def test_string(options):
     '''
     test of core/plugins/string.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_string.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_string.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
-def test_on_exit():
+def test_on_exit(options):
     '''
     test of core/Chain.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
-    session = shell("python -u goephor.py -f ./examples/ex_on_exit.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_on_exit.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
-def test_include():
+def test_include(options):
     '''
     test of core/plugins/system.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
     sys.stdout.flush()
-    session = shell("python -u goephor.py -f ./examples/ex_include.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_include.yaml -e -E %s" % options.envs)
     if not session.get('code') == 0:
         return {funct: False}
     return {funct: True}
 
-def test_fail():
+def test_fail(options):
     '''
     test of core/plugins/system.py
     '''
     funct = inspect.stack()[0][3]
     print "\n\n[%s]\n" % (funct)
     sys.stdout.flush()
-    session = shell("python -u goephor.py -f ./examples/ex_fail.yaml -e")
+    session = shell("python -u goephor.py -f ./examples/ex_fail.yaml -e -E %s" % options.envs)
     if not session.get('code') > 0:
         return {funct: False}
     return {funct: True}
 
-def tests():
+def tests(options):
     '''
     calls all the tests here & collects results
     '''
     tests = []
-    tests.append(test_condition())
-    tests.append(test_defaults())
-    tests.append(test_environment())
-    tests.append(test_freebsd())
-    tests.append(test_http())
-    tests.append(test_receipt())
-    tests.append(test_remote())
-    tests.append(test_scm())
-    tests.append(test_system())
-    tests.append(test_release())
-    tests.append(test_string())
-    tests.append(test_on_exit())
-    tests.append(test_fail())
-    tests.append(test_include())
+    tests.append(test_condition(options))
+    tests.append(test_defaults(options))
+    tests.append(test_environment(options))
+    tests.append(test_freebsd(options))
+    tests.append(test_http(options))
+    tests.append(test_receipt(options))
+    tests.append(test_remote(options))
+    tests.append(test_scm(options))
+    tests.append(test_system(options))
+    tests.append(test_string(options))
+    tests.append(test_on_exit(options))
+    tests.append(test_fail(options))
+    tests.append(test_include(options))
     print "\n\n[Test Results]:"
     for test in tests:
         key = test.keys()[0]
@@ -220,4 +241,5 @@ def tests():
 
 
 if __name__ == '__main__':
-    tests()
+    options = menu()
+    tests(options)
