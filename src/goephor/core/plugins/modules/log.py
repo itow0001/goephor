2fe985450390484defa13e2bee2fdb1727f73137
iitow <iitow@isilon.com>
2016-07-26 00:47:29 -0700

    debug log management

--- src/goephor/core/plugins/modules/log.py
+++ src/goephor/core/plugins/modules/log.py
@@ -30,16 +30,17 @@ def message(message_type,output,debug=False):
     :return: colorized string
     '''
     #now = time.strftime("%c")
+    now = ""
     try:
         if debug:
-            with open("DEBUG.log", "a") as debug:
-                output_str = "%s\n" % (output)
+            with open("debug.log", "a") as debug:
+                output_str = "%s # %s\n" % (str(now),output)
                 debug.write(output_str)
         output_final = colors(message_type,output)
         sys.stdout.flush()
     except Exception as e:
-        with open("DEBUG.log", "a") as debug:
-            output_str = "%s\n" % (output)
+        with open("debug.log", "a") as debug:
+            output_str = "%s # %s\n" % (str(now),output)
             debug.write(output_str)
         return "[Error] plugin/modules/log %s" % str(e)
     return output_final
